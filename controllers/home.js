400820337521e761b420e936f1efca7f33d3c646
oliverwanyonyi <oliverwanyonyi02@gmail.com>
2021-09-16 15:57:10 +0300

    minor-bug

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,7 @@
 const Posts = require("../models/posts");
 const User = require("../models/user");
 exports.getHomePage = (req, res, next) => {
-  User.find(req.session.user._id).then((users) => {
+  User.find().then((users) => {
     Posts.find().then((posts) => {
       res.render("home/index", {
         posts: posts,
